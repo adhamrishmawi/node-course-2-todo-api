aa81e2a8a2add921f9605b5e881677f787ef5924
Adham Rishmawi <Adham@Adhams-MacBook-Pro.local>
2018-06-17 14:01:48 +0300

    Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -22,9 +22,11 @@ app.post('/todos', (req, res) => {
 
 
 
-app.listen(3001, ()=>{
-  console.log('Started on port 3001');
+app.listen(3000, ()=>{
+  console.log('Started on port 3000');
 });
+
+module.exports = {app};
 
 
 
