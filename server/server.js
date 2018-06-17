90ec17074d7a44763f41a3e44a5cdf6ae9a7abb5
Adham Rishmawi <Adham@Adhams-MacBook-Pro.local>
2018-06-17 17:22:51 +0300

    Add tests for GET /todos

--- server/server.js
+++ server/server.js
@@ -20,6 +20,13 @@ app.post('/todos', (req, res) => {
   });
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos}); //could do res.send(todos) but this would return an array which is limiting. Do it with {todos: todos} or in ES6 {todos} to return it an as object
+  }, (e)=> {
+    res.status(400).send(e);
+  });
+});
 
 
 app.listen(3000, ()=>{
