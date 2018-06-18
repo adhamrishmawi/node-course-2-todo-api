f6c2786f7874fbe69d14a9556a403a91f85e4d6f
Adham Rishmawi <Adham@Adhams-MacBook-Pro.local>
2018-06-18 20:08:32 +0300

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var {ObjectID} = require('mongodb');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -67,6 +68,32 @@ app.delete('/todos/:id', (req, res) => {
 
 app.listen(port, ()=>{
   console.log(`Started on port ${port}`);
+});
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']); // pick is a lodash method that allows us to pick which properties the user gets to update. For example, we do not want the user to be able to update Completed At. That is something for the server to update
+
+  if (!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed){
+    body.completedAt = new Date().getTime(); //returns a javascript timestamp; number of seconds since midnight on january 1 1970.
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body},{new: true}).then((todo)=>{
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e)=>{
+    res.status(400).send();
+  });
+  // new: true in order to get the new item returned not the old one
 });
 
 module.exports = {app};
