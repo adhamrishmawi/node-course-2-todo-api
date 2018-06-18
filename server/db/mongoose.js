911f8f17546de7ec1f6d01dcc201749b687ef291
Adham Rishmawi <Adham@Adhams-MacBook-Pro.local>
2018-06-18 18:25:15 +0300

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,15 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise; //there is a preference here to use promises as opposed to callbacks
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+let db = {
+  localhost: 'mongodb://localhost:27017/TodoApp',
+  mlab: 'mongodb://adham:s3ld0m@ds161620.mlab.com:61620/db-for-nodejs-course'
+};
+
+mongoose.connect(db.localhost || db.mlab);
+
+//mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 // does not require a callback because mongoose is more sophisticated and will wait to connec to the database before attempting to do anything else
 
 module.exports = {mongoose};
--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
+//setting up for Hiroku
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -49,8 +51,8 @@ app.get('/todos/:id', (req, res)=> { //will create an id variable that will be o
 });
 
 
-app.listen(3000, ()=>{
-  console.log('Started on port 3000');
+app.listen(port, ()=>{
+  console.log(`Started on port ${port}`);
 });
 
 module.exports = {app};
