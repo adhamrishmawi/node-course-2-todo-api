2919978b722e8ba1cc6cb1f367b714a1c7a17520
Adham Rishmawi <Adham@Adhams-MacBook-Pro.local>
2018-06-24 20:49:04 +0300

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,31 @@
 const {SHA256} = require('crypto-js');  //this is requiring a certain property (SHA256) of the return result
-
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-};
+var password = '123abc';
 
-var token = jwt.sign(data, '123abc'); //123abc is the secret
-console.log(token);
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) =>{
+//     console.log(hash); //hash is the value that we will save to the database
+//   });
+// });
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+var hashedPassword = '$2a$10$l1aVngF2HTEYzkcCX8lF1.AZn83LaYFraqhJc30E/88AUVY2d82Hq';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
+
+
+// var data = {
+//   id: 10
+// };
+
+// var token = jwt.sign(data, '123abc'); //123abc is the secret
+// console.log(token);
+
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
 
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString(); //toString because the result is an object
